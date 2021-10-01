532bbb6dbc49199dddc2f3a0e85c90f2b0ddaf59
Ale <alearanibar@gmail.com>
2021-09-30 23:41:31 -0700

    homepage complete, working on adminPlayer component

---
+++ src/components/Home/promotion/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Enroll from "./Enroll";
+import Animation from "./Animation";
+
+const Promotion = () => {
+  return (
+    <div className="promotion_wrapper">
+      <div className="container">
+        <Animation />
+        <Enroll />
+      </div>
+    </div>
+  );
+};
+
+export default Promotion;
